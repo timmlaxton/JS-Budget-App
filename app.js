8264ca18ae6bb08ed37fe80de0d90b5d41fafc2e
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-09-08 14:22:09 +0100

    added expenses and income to DOM

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ let UIController = (function(){
       inputValue: '.add__value',
       inputBtn: '.add__btn',
       incomeContainer: '.income__list',
-      expesneContainer: '.expense__list',
+      expensesContainer: '.expenses__list',
     }
 
   return {
@@ -103,7 +103,7 @@ let UIController = (function(){
 
           html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
         } else if (type === 'exp') {
-          element = DOMstrings.expesneContainer;
+          element = DOMstrings.expensesContainer;
 
       html =  '<div class="item clearfix" id="expense-%id&"><divclass="item__description">%description%</divclass=><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div></div></div>';
         }                   
